8f2561c41f699daa5a65f96b4a7672e0d23ea6d4
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-02 22:03:51 +0300

    User has many sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :sessions, dependent: :destroy
+
   validates :email, presence: true
 
   validates :first_name, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe User do
   it { should be_a(ApplicationRecord) }
 
+  it { should have_many(:sessions).dependent(:destroy) }
+
   it { should validate_presence_of(:email) }
 
   it { should validate_presence_of(:first_name) }
