40afd8558e4acfc68bb289b29094fab3b9db7464
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-02 21:54:58 +0300

    Add session model

---
+++ db/migrate/20170402185035_create_sessions.rb
@@ -0,0 +1,10 @@
+class CreateSessions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :sessions do |t|
+      t.integer :user_id
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User do
+  it { should be_a(ApplicationRecord) }
+
   it { should validate_presence_of(:email) }
 
   it { should validate_presence_of(:first_name) }
