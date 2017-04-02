08a0f032f0521279a75b58075e00d3a7c80f3c3b
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-02 21:49:51 +0300

    Add routes and routing specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: { format: 'json' } do
+    resources :docs, only: :index
+
+    resource :sign_up, only: :create
+
+    resource :sign_in, only: :create
+
+    resource :sign_out, only: :destroy
+
+    resource :profile, only: :show
+  end
 end
