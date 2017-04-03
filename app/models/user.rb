0391086ef78cf36d0a27c4477f3bda29a150a5d0
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-03 20:54:51 +0300

    Validate user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   validates :email, presence: true
 
+  validates :email, uniqueness: { case_sensitive: false }
+
   validates :first_name, presence: true
 
   validates :last_name, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,8 @@ describe User do
 
   it { should validate_presence_of(:email) }
 
+  it { should validate_uniqueness_of(:email).case_insensitive }
+
   it { should validate_presence_of(:first_name) }
 
   it { should validate_presence_of(:last_name) }
